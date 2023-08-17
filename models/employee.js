98052c22b24fb794ea616725bfcea29da5eb8006
Yavnesh <yavnesh.sharma@gmail.com>
2023-08-17 16:33:12 +0530

    fixed required field for end date

--- models/employee.js
+++ models/employee.js
@@ -5,7 +5,7 @@ const employeeSchema = new mongoose.Schema({
   employee_name:{type: String, required: true },
   employee_designation: { type: String, required: true },
   start_date: { type: Date, required: true },
-  end_date: { type: Date, required: true },
+  end_date: { type: Date},
 }, { timestamps: true });
 
 const Employee = mongoose.model('Employee', employeeSchema);
